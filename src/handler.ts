2ed0611948425bbff4c8c8c3ca16d1ae00c2fd1e
Henrique Melo <henrique.melo@oryon.solutions>
2022-05-19 18:13:23 +0100

    Updates

--- src/handler.ts
+++ src/handler.ts
@@ -10,19 +10,24 @@ export async function handleRequest(request: Request): Promise<Response> {
 
 
   const auth = new Auth(request.headers.get('authorization')!);
-  let isValid = await auth.isValidJwt();
+  const isValid = await auth.isValidJwt();
   if (!isValid) return  ResponseMessage(request, "Unauthorized", 401, null);
 
   const collection = "property_types";
 
+
   const mongoClient = new MongoAtlasDataApiSDK(
-    // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       MONGO_API_KEY,
-    // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       MONGO_URL,
-    // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       MONGO_DATA_SOURCE,
-    // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       MONGO_DATABASE,
     false
   );
@@ -30,84 +35,10 @@ export async function handleRequest(request: Request): Promise<Response> {
 
   try {
 
-    if (path === "/edit") {
-      let propertyType : PropertyType = await request.json()
-      let findParams : FindOneParams = {
-        collection: collection,
-        filter:  { "name": propertyType.name }
-      }
-      let exists = await mongoClient.findOne(findParams);
-      if(exists.document === null || exists.document["_id"] === propertyType._Id) {
-        let updateParams : UpdateParams = {
-          collection: collection,
-          filter: { "_id": { "$oid": propertyType._Id } },
-          update: {
-            "$set": {
-              "name": propertyType.name,
-              "active": propertyType.active
-            }
-          },
-          upsert: false
-        }
-        const res = await mongoClient.updateOne(updateParams);
-
-        // TODO: Alterar resposta
-        return ResponseMessage(request, JSON.stringify({"" : res}), 200, null);
-      }
-      else{
-        return ResponseMessage(request, JSON.stringify({"" : ""}), 409, `Já existe um tipo de propriedade com o nome ${propertyType.name}`);
-      }
-    }
-
-    if (path === "/create") {
-      if (auth.validatePermissions("create:property_type")) {
-
-        const propertyType : CreatePropertyType = await request.json();
-        let existsParams : FindOneParams = {
-          collection: collection,
-          filter: { "name": propertyType.name },
-        }
-
-        let exists = await mongoClient.findOne(existsParams);
-
-        if(exists.document === null){
-          let res = await mongoClient.insertOne({
-            collection: collection,
-            document: {"name": propertyType.name, "active": true}
-          });
-
-          // TODO: Alterar resposta
-          return ResponseMessage(request, JSON.stringify({"" : ""}), 200, null);
-        }
-        else{
-          return ResponseMessage(request, JSON.stringify({"" : ""}), 409, `Já existe um tipo de propriedade com o nome ${propertyType.name}`);
-        }
-      } else {
-        return ResponseMessage(request, JSON.stringify({"error": "You do not have access!"}), 403, null);
-      }
-    }
-
-    if (path === "/get-all") {
-      // Model of response
-      if (auth.validatePermissions("read:property_types")) {
-        const tableOptions : TableOptions = await request.json();
-
-        let findParams : FindParams = {
-          collection: collection,
-          skip: tableOptions.pageSize * tableOptions.page,
-          limit: tableOptions.pageSize,
-          filter: tableOptions.search === null || tableOptions.search.match(/^ *$/) !== null ? null : {"name": {"$regex": tableOptions.search, "$options": "i"}},
-        }
-
-        let response = await mongoClient.find(findParams);
-        let totalItems = await mongoClient.find({collection, filter:  tableOptions.search === null || tableOptions.search.match(/^ *$/) !== null ? null : {"name": {"$regex": tableOptions.search, "$options": "i"}}});
-
-        return ResponseMessage(request, JSON.stringify({ "documents": response.documents, "total": totalItems.documents.length } ), 200, null);
-      } else {
-        return ResponseMessage(request, JSON.stringify({"error": "You do not have access!"}), 403, null);
-      }
-      // Get one or multiple
-    }
+    // if(path === "/some-path"){
+    //   return ResponseMessage(request, "Some message", 200, null);
+    // Do something if the path is /some-path
+    //}
 
     return new Response('Not Allowed', {status: 405});
 
